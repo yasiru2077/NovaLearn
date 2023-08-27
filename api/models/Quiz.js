031a992e6d86d29bdfbfefdd681fd05559e8d514
yasiru2077 <yasiruudana1999@gmail.com>
2023-08-27 15:40:53 +0530

    update

---
+++ api/models/Quiz.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const questionSchema = new mongoose.Schema({
+    questionText: {
+        type: String,
+        required: true
+    },
+    options: {
+        type: [String],
+        required: true
+    },
+    correctOptionIndex: {
+        type: Number,
+        required: true
+    }
+});
+
+const quizSchema = new mongoose.Schema({
+    
+
+    teaching_subject:{
+        type: String,
+        required:true 
+    },
+    
+
+    title: {
+        type: String,
+        required: true
+    },
+    questions: {
+        type: [questionSchema],
+        required: true
+    }
+});
+
+const Quiz = mongoose.model("Quiz", quizSchema);
+
+module.exports = Quiz;
