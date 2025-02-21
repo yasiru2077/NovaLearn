f10070a276fab4d94dc75d35816537e383cd3af9
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2025-02-21 16:06:42 +0530

    changing the files

--- server/middleware.js
+++ server/middleware.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import { db } from "../connect.js";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.accessToken;
@@ -12,6 +13,25 @@ export const verifyToken = (req, res, next) => {
       return res.status(403).json("Token is not valid!");
     }
     req.userId = userInfo.id;
+    next();
+  });
+};
+
+export const verifyAdmin = (req, res, next) => {
+  const q = "select role from lmsAuth where id = ?";
+
+  db.query(q, [req.userId], (err, data) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+
+    if (data.length === 0) {
+      return res.status(404).json("User not found!");
+    }
+
+    if (data[0].role !== "admin") {
+      return res.status(403).json("Access denied! Admins only.");
+    }
     next();
   });
 };
