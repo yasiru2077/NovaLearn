b23ab95ca756191b7dc08c6f64f1ca4edcf62b29
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2025-03-16 18:05:20 +0530

    frontend usermanagement

--- client/src/content/main-navbar/main-navbar.jsx
+++ client/src/content/main-navbar/main-navbar.jsx
@@ -1,6 +1,9 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link, Links, useActionData, useNavigate } from "react-router-dom";
+import navIcon from "../../assets/menu-alt-1-svgrepo-com.svg";
+// import navAfterIcon from "../../assets/menu-alt-svgrepo-com.svg";
+import navAfterIcon from "../../assets/menu-alt-1-svgrepo-com (1).svg";
 import "./main-nav.css";
 import NotFound from "../not-found";
 
@@ -10,6 +13,12 @@ function MainNavbar({ setIsAuthenticated, userDetails, isAuthenticated }) {
     role: userDetails.role,
     email: userDetails.email,
   });
+
+  const [activateNav, setActivateNav] = useState(false);
+
+  const handleNav = () => {
+    setActivateNav(!activateNav);
+  };
 
   console.log(userDetails);
 
@@ -39,48 +48,64 @@ function MainNavbar({ setIsAuthenticated, userDetails, isAuthenticated }) {
   // }, [isAuthenticated]);
 
   return (
-    <section className="main-nav-bar">
-      <div>
-        <h1 className="logo">[NOVALEARN]</h1>
-      </div>
-      <div className="page-links">
-        {user.role === "admin" ? (
-          <ul>
-            <li>Home</li>
-            <li>
-              <Link to={`/announcement`}>Announcement</Link>
-            </li>
-            <li>
-              <Link to={`/user-management`}> User & Course Management</Link>
-            </li>
-            <li>
-              <Link to={`/enrollment`}>Enrollments</Link>
-            </li>
-          </ul>
-        ) : user.role === "lecturer" ? (
-          <ul>
-            <li>Home</li>
-          </ul>
-        ) : user.role === "student" ? (
-          <ul>
-            <li>Home</li>
-          </ul>
+    <React.Fragment>
+      <section
+        className={`mobile-nav ${activateNav ? "mobile-nav-drop-down" : ""}`}
+      >
+        <div>
+          <h1 className="logo">[NOVALEARN]</h1>
+        </div>
+
+        {activateNav === true ? (
+          <img src={navIcon} onClick={handleNav} alt="" />
         ) : (
-          ""
+          <img src={navAfterIcon} onClick={handleNav} alt="" />
         )}
-      </div>
-      <div className="logout-container">
-        <ul>
-          <div>
-            <li className="user-name">{user.username}</li>
-            <span className="role">{user.role}</span>
-          </div>
-          <li onClick={handleLogOut} style={{ cursor: "pointer" }}>
-            logout
-          </li>
-        </ul>
-      </div>
-    </section>
+      </section>
+
+      <section className="main-nav-bar">
+        <div>
+          <h1 className="logo">[NOVALEARN]</h1>
+        </div>
+        <div className="page-links">
+          {user.role === "admin" ? (
+            <ul>
+              <li>Home</li>
+              <li>
+                <Link to={`/announcement`}>Announcement</Link>
+              </li>
+              <li>
+                <Link to={`/user-management`}> User & Course Management</Link>
+              </li>
+              <li>
+                <Link to={`/enrollment`}>Enrollments</Link>
+              </li>
+            </ul>
+          ) : user.role === "lecturer" ? (
+            <ul>
+              <li>Home</li>
+            </ul>
+          ) : user.role === "student" ? (
+            <ul>
+              <li>Home</li>
+            </ul>
+          ) : (
+            ""
+          )}
+        </div>
+        <div className="logout-container">
+          <ul>
+            <div>
+              <li className="user-name">{user.username}</li>
+              <span className="role">{user.role}</span>
+            </div>
+            <li onClick={handleLogOut} style={{ cursor: "pointer" }}>
+              logout
+            </li>
+          </ul>
+        </div>
+      </section>
+    </React.Fragment>
   );
 }
 
